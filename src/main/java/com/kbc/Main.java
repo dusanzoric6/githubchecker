b741e5bd323e2b65bcb58f23e798745a6d42c1d3
gleb <mendrul.gleb@gmail.com>
2019-08-28 20:18:03 +0100

    refactoring

--- src/main/java/com/kbc/Main.java
+++ src/main/java/com/kbc/Main.java
@@ -6,12 +6,12 @@ import com.kbc.service.GithubChecker;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length != 2) {
+        String searchString = System.getProperty("search");
+        String inputTag = System.getProperty("tag");
+
+        if (searchString == null || inputTag == null) {
             throw new IllegalArgumentException("search string or tag is missing");
         }
-
-        String searchString = args[0];
-        String inputTag = args[1];
 
         GithubChecker githubChecker = new GithubChecker(new GithubCheckerClient());
         githubChecker.start(searchString, inputTag);
